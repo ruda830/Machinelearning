e5f1780730716f369ce071e4e0067bdb2e043d31
ruda <sheimisky7150@yahoo.co.jp>
2021-02-14 22:59:45 +0900

    ヒートマップ

--- test_4.R
+++ test_4.R
@@ -1,5 +1,10 @@
 data(USArrests)
+#デンドログラム
 #distで相互の変数間の距離を求める
 USArrests.d <- dist(USArrests)
 USArrests.hc <- hclust(USArrests.d)
 plot(USArrests.hc)
+
+#ヒートマップ
+data(USArrests)
+heatmap(as.matrix(USArrests))
