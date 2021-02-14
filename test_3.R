5e8699575a5d23728dd2468c4bba23c0e5f9f151
ruda <sheimisky7150@yahoo.co.jp>
2021-02-14 22:55:45 +0900

    確率質量関数　プロット追加

--- test_3.R
+++ test_3.R
@@ -5,3 +5,10 @@
 #確率質量関数
 par(ann=F)
 plot(1:50, dpois(1:50, lambda = 1), type="l", ylim=c(0,0.4), xlim=c(0, 25))
+
+par(new=T)
+plot(1:50, dpois(1:50, lambda = 4), type="l", ylim=c(0,0.4), xlim=c(0, 25))
+
+par(new=T)
+plot(1:50, dpois(1:50, lambda = 10), type="l", ylim=c(0,0.4), xlim=c(0, 25))
+
